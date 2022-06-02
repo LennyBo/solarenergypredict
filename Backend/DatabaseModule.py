6096e9dd665a04f1a4acf82d6d00977747905478
LennyBo <lenny.boegli@moosvolk.ch>
2022-06-02 16:37:25 +0200

    Updated deepl to implement early stopping and added historic energy into db

--- Backend/DatabaseModule.py
+++ Backend/DatabaseModule.py
@@ -2,6 +2,7 @@ import sqlite3
 from traceback import print_tb
 import pandas as pd
 from requests import delete
+from datetime import date
 
 
 class DatabaseModule:
@@ -11,25 +12,24 @@ class DatabaseModule:
             self.delete_table()
         self.create_table()
         
-
-    def insert_data(self,dict):
+    def insert_data(self,data):
         db = sqlite3.connect(self.database_name)
-        db.execute(f'''INSERT INTO SolarData 
+        db.execute(f'''INSERT INTO HistoricPower 
                    (time,solar_power, grid_power, house_power, twc_power, heater_power, heater_mode) 
-                   VALUES ("{dict['time']}",{dict['solar_power']}, {dict['grid_power']}, {dict['house_power']}, {dict['twc_power']}, {dict['heater_power']}, "{dict['heater_mode']}")'''
+                   VALUES ("{data['time']}",{data['solar_power']}, {data['grid_power']}, {data['house_power']}, {data['twc_power']}, {data['heater_power']}, "{data['heater_mode']}")'''
                    )
         db.commit()
         db.close()
         
     def select_data_day(self,date):
         db = sqlite3.connect(self.database_name)
-        df = pd.read_sql(f'''SELECT * FROM SolarData WHERE DATE(time)='{date}' ''', db)
+        df = pd.read_sql(f'''SELECT * FROM HistoricPower WHERE DATE(time)='{date}' ''', db)
         db.close()
         return df
     
     def create_table(self):
         db = sqlite3.connect(self.database_name)
-        db.execute('''CREATE TABLE IF NOT EXISTS SolarData (
+        db.execute('''CREATE TABLE IF NOT EXISTS HistoricPower (
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
                 time DATETIME NOT NULL,
                 solar_power INTERGER,
@@ -40,9 +40,54 @@ class DatabaseModule:
                 heater_mode TEXT
                 )'''
             )
+        db.execute('''CREATE TABLE IF NOT EXISTS DailyEnergy (
+                date DATE PRIMARY KEY DEFAULT CURRENT_DATE,
+                solar_energy INTERGER,
+                grid_energy INTERGER,
+                twc_energy INTERGER,
+                twc_green_precentage FLOAT,
+                heater_energy INTERGER,
+                heater_green_precentage FLOAT,
+                house_energy INTERGER,
+                house_green_precentage FLOAT
+                )'''
+            )
         db.close()
     
     def delete_table(self):
         db = sqlite3.connect(self.database_name)
-        db.execute('''DROP TABLE IF EXISTS SolarData''')
+        db.execute('''DROP TABLE IF EXISTS HistoricPower''')
+        db.execute('''DROP TABLE IF EXISTS DailyEnergy''')
         db.close()
+        
+    def insert_daily_energy(self,data,date=date.today()):
+        db = sqlite3.connect(self.database_name)
+        db.execute(f'''REPLACE INTO DailyEnergy
+                   (date,solar_energy,grid_energy, twc_energy, twc_green_precentage, heater_energy, heater_green_precentage, house_energy,house_green_precentage)
+                   VALUES ("{date}",{data['solar_energy']}, {data['grid_energy']},
+                   {data['twc_energy']}, {data['twc_green_precentage']},
+                   {data['heater_energy']}, {data['heater_green_precentage']},
+                   {data['house_energy']}, {data['house_green_precentage']}
+                   )'''
+                )
+        db.commit()
+        db.close()
+    
+    def select_daily_energy(self,date=date.today()):
+        db = sqlite3.connect(self.database_name)
+        df = pd.read_sql(f'''SELECT * FROM DailyEnergy''', db)
+        db.close()
+        return df
+
+if __name__ == '__main__':
+    db = DatabaseModule('data/SolarDatabase.db',True)
+    
+    db.insert_daily_energy({'solar_energy':100,'grid_energy':200,'twc_energy':300,'twc_green_precentage':0.5,'heater_energy':400,'heater_green_precentage':0.6,'house_energy':500,'house_green_precentage':0.7})
+    db.insert_daily_energy({'solar_energy':50,'grid_energy':200,'twc_energy':300,'twc_green_precentage':0.5,'heater_energy':400,'heater_green_precentage':0.6,'house_energy':500,'house_green_precentage':0.7})
+    db.insert_daily_energy({'solar_energy':50,'grid_energy':33,'twc_energy':300,'twc_green_precentage':0.5,'heater_energy':400,'heater_green_precentage':0.6,'house_energy':500,'house_green_precentage':0.7})
+    db.insert_daily_energy({'solar_energy':50,'grid_energy':200,'twc_energy':100,'twc_green_precentage':0.5,'heater_energy':400,'heater_green_precentage':0.6,'house_energy':500,'house_green_precentage':0.7})
+    
+    df = db.select_daily_energy()
+    print(df)
+    
+    
--- DeepLearning/DeepLSolar.py
+++ DeepLearning/DeepLSolar.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 from DataEngine import loadDFsToCrunch,Preprocessing
-
+import keras
 from keras import layers
 from keras.models import Sequential
 from keras.layers import Dense
@@ -18,6 +18,44 @@ def getBaselines(x, y):
     """Returns mae and mse of the 2 arrrays"""
     return (round(mean_absolute_error(x, y), roundDecimal), round(mean_squared_error(x, y), roundDecimal))
 
+def transformer_encoder(inputs, head_size, num_heads, ff_dim, dropout=0):
+    # Normalization and Attention
+    x = layers.LayerNormalization(epsilon=1e-6)(inputs)
+    x = layers.MultiHeadAttention(
+        key_dim=head_size, num_heads=num_heads, dropout=dropout
+    )(x, x)
+    x = layers.Dropout(dropout)(x)
+    res = x + inputs
+
+    # Feed Forward Part
+    x = layers.LayerNormalization(epsilon=1e-6)(res)
+    x = layers.Conv1D(filters=ff_dim, kernel_size=1, activation="relu")(x)
+    x = layers.Dropout(dropout)(x)
+    x = layers.Conv1D(filters=inputs.shape[-1], kernel_size=1)(x)
+    return x + res
+
+def build_model(
+    input_shape,
+    head_size,
+    num_heads,
+    ff_dim,
+    num_transformer_blocks,
+    mlp_units,
+    dropout=0,
+    mlp_dropout=0,
+):
+    inputs = keras.Input(shape=input_shape)
+    x = inputs
+    x = layers.BatchNormalization()(x)
+    for _ in range(num_transformer_blocks):
+        x = transformer_encoder(x, head_size, num_heads, ff_dim, dropout)
+
+    x = layers.GlobalAveragePooling1D(data_format="channels_first")(x)
+    for dim in mlp_units:
+        x = layers.Dense(dim, activation="relu")(x)
+        x = layers.Dropout(mlp_dropout)(x)
+    outputs = layers.Dense(1)(x)
+    return keras.Model(inputs, outputs)
 
 def printBaseLine(y, roundDecimal=2):
     """prints baselines of the y axis
@@ -63,7 +101,7 @@ def plot_metrics(history, metrics):
 
 
 if __name__ == "__main__":
-    EPOCHS = 55
+    EPOCHS = 200
     BATCH_SIZE = 256
     TEST_SIZE = 0.16 # Precent
     VAL_SIZE = 360 # Days
@@ -114,15 +152,35 @@ if __name__ == "__main__":
     model.add(layers.Dropout(0.2))
     model.add(Dense(1))
 
+   
+    input_shape = X_train.shape[1:]
+
+    # model = build_model(
+    #     input_shape,
+    #     head_size=256,
+    #     num_heads=4,
+    #     ff_dim=4,
+    #     num_transformer_blocks=4,
+    #     mlp_units=[128],
+    #     mlp_dropout=0.4,
+    #     dropout=0.25,
+    # )
     model.compile(loss="mean_squared_error", optimizer="adam",
                   metrics=["mean_absolute_error"])
     
+    
     print(f"Training with epochs={EPOCHS} batch_size={BATCH_SIZE}...\n")
 
+    callbacks = [keras.callbacks.EarlyStopping(patience=20, restore_best_weights=True)]
+    
+    
     trainHistory = model.fit(
-        X_train, y_train, 
-        epochs=EPOCHS, batch_size=BATCH_SIZE, 
-        verbose=1,validation_data=(X_val, y_val)
+        X_train,
+        y_train,
+        epochs=EPOCHS,
+        batch_size=BATCH_SIZE,
+        validation_data=(X_val, y_val),
+        callbacks=callbacks,
     )
 
     score = model.evaluate(X_test, y_test, verbose=0)
